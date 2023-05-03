3b830b94ac8d06e02822d47028e926b566b40123
Jose <jalixder@gmail.com>
2023-05-02 22:31:37 -0500

    Se realiza fincion de desencriptar en JavaScript, se verifica configuracion de boton

--- Desktop/Challenge/Encriptador.js
+++ Desktop/Challenge/Encriptador.js
@@ -18,3 +18,24 @@ function encriptar(){
         alert("POR FAVOR INGRESE ALGUN TEXTO!!!")
     }        
 }
+
+function desencriptar(){
+    var frase=document.getElementById("texto").value.toLowerCase();
+   
+    var texto = frase.replace(/enter/mg,"e");
+    var texto = texto.replace(/imes/mg,"i");
+    var texto = texto.replace(/ai/mg,"a");
+    var texto = texto.replace(/ober/mg,"o");
+    var texto = texto.replace(/ufat/mg,"u");
+
+    if (texto.length > 0) {
+        document.getElementById("muneco").style.display="none";
+        document.getElementById("parrafo").style.display="none";
+        document.getElementById("titulo-mensaje").style.display="none";
+        document.getElementById("mensaje").style.display="flexbox";
+        document.getElementById("mensaje").innerHTML=texto;
+        var btn2 = document.querySelector("#btn-desencriptar");                 
+    } else{
+        alert("POR FAVOR INGRESE ALGUN TEXTO!!!")
+    }        
+}
